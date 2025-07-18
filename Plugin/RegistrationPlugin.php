72a9d283c9abad74248215c1b2e10634f9b458d8
Claudio Ferraro <claudio@justbetter.nl>
2025-07-18 11:13:11 +0200

    Changed module vendor from sbodak to claudioferraro for continued independent maintenance

--- Plugin/RegistrationPlugin.php
+++ Plugin/RegistrationPlugin.php
@@ -1,56 +1,27 @@
 <?php
-/**
- * @package   Bodak\DisableRegistration
- * @author    Slawomir Bodak <slawek.bodak@gmail.com>
- * @copyright © 2017 Slawomir Bodak
- * @license   See LICENSE file for license details.
- */
-
 declare(strict_types=1);
 
-namespace Bodak\DisableRegistration\Plugin;
+namespace ClaudioFerraro\DisableRegistration\Plugin;
 
 use Magento\Customer\Model\Registration;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
-/**
- * Class RegistrationPlugin
- *
- * @category Plugin
- * @package  Bodak\DisableRegistration\Plugin
- */
 class RegistrationPlugin
 {
-    /**
-     * ScopeConfigInterface
-     *
-     * @var ScopeConfigInterface
-     */
-    protected $scopeConfig;
-
     const XML_PATH_DISABLE_CUSTOMER_REGISTRATION = 'customer/create_account/disable_customer_registration';
 
-    /**
-     * RegistrationPlugin constructor.
-     *
-     * @param ScopeConfigInterface $scopeConfig ScopeConfigInterface
-     */
-    public function __construct(ScopeConfigInterface $scopeConfig)
-    {
-        $this->scopeConfig = $scopeConfig;
+    public function __construct(
+        protected ScopeConfigInterface $scopeConfig
+    ) {
     }
 
     /**
      * Check if registration is allowed
-     *
-     * @param Registration $subject Registration
-     *
-     * @return bool
      */
     public function afterIsAllowed(Registration $subject): bool
     {
-        return !$this->scopeConfig->getValue(
+        return !$this->scopeConfig->isSetFlag(
             self::XML_PATH_DISABLE_CUSTOMER_REGISTRATION,
             ScopeInterface::SCOPE_STORE
         );
--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Magento 2 - Disable customer registration
- 
+
+## About this fork
+This module is a fork of [sbodak/magento2-b2b-disable-customer-registration](https://github.com/sbodak/magento2-b2b-disable-customer-registration). The original module by Bodak is no longer maintained, so I continue development and maintenance here.
+
 ## Overview
 This module enable possibility to disable the customer registration.
 It can be useful for B2B customers, if we don't want to allow them register by themselves. 
 Extension will remove link to register page and registration form from login page.
 
 ## Compatibility
-- Magento 2.1.x - 2.3.x
+- Magento 2.4.x
+- PHP >=8.0
 
 ## Installation details
-1. Run `composer require sbodak/magento2-b2b-disable-customer-registration`
-2. Run `php bin/magento module:enable Bodak_DisableRegistration`
+1. Run `composer require claudioferraro/magento2-b2b-disable-customer-registration`
+2. Run `php bin/magento module:enable ClaudioFerraro_DisableRegistration`
 3. Run `bin/magento setup:upgrade`
 4. Run `bin/magento clean:cache`
 
@@ -29,7 +33,7 @@ Extension will remove link to register page and registration form from login pag
 
 
 ## Uninstall
-1. Run `composer remove sbodak/magento2-b2b-disable-customer-registration`
+1. Run `composer remove claudioferraro/magento2-b2b-disable-customer-registration`
 
 ## License
 [MIT License](LICENSE)
